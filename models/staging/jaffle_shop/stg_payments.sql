439cfd847179c8a19fe119b27ff1f7bca0c84470
aj.lawal@intuitaconsulting.com <aj.lawal@intuitaconsulting.com>
2023-05-11 14:53:44 +0000

    Working through course chapters

---
+++ models/staging/jaffle_shop/stg_payments.sql
@@ -0,0 +1,15 @@
+with payments as (
+    select  id as payment_id,
+        order_id as order_id,
+        payment_method as payment_method,
+        status,
+
+        -- amount is stored in cents, convert it to dollars
+        amount / 100 as amount,
+        created as created_at
+
+    from {{ source('jaffle_shop', 'payment') }}
+)
+
+    select *
+    from payments
